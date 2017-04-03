db9824b39ba26b3bdcc5367f71206ba74a27aad5
Daniele Perilli <daniele.perilli@gmail.com>
2017-04-03 17:50:46 +0200

    iOS rendering issue fix

--- src/colorblind.ts
+++ src/colorblind.ts
@@ -18,7 +18,11 @@ module powerbi.extensibility.visual {
             element.selectAll('#visionFilter').remove();
             element.append('svg')
                 .attr('id', 'visionFilter')
-                .style('position', 'absolute')
+                .style({
+                    'position': 'absolute',
+                    'width': 0,
+                    'height': 0
+                })
                 .html(visionDefs);
 
             element.style({
